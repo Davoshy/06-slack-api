366bbed335433fb6f219348c61cd44bee9f4b8a1
Davoshy <44115294+Davoshy@users.noreply.github.com>
2020-02-27 14:30:35 +0800

    finished day1 users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,31 @@
 // Require
-const Users = require('../models/users')
-const router = require('express').Router()
+const Users = require("../models/users");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // Routes
-router.post('/signup', (req, res) => {})
-router.post('/login', (req, res) => {})
+router.post("/signup", (req, res) => {
+  Users.findOne({ email: req.body.email })
+    .then(response => {
+      if (response == null) {
+        let encrypted = bcrypt.hashSync(req.body.password, 10);
+        req.body.password = encrypted;
+        Users.create(req.body).then(user => {
+          let u = user.toObject();
+          let token = jwt.sign(u, process.env.secret);
+          res.send(token);
+          localStorage.setItem("token", token);
+        });
+      } else {
+        res.send("Email already in use");
+      }
+    })
+    .catch(err => console.log(err));
+});
+
+router.post("/login", (req, res) => {});
 
 // Export
-module.exports = router
+module.exports = router;
